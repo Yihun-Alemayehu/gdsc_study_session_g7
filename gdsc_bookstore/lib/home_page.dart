ff14a6fadfe7ce9d974992b48eca6e1d295fd7b0
Yihun Alemayehu <yankure01@gmail.com>
2023-12-25 15:30:52 +0300

    first commit

--- gdsc_bookstore/lib/home_page.dart
+++ gdsc_bookstore/lib/home_page.dart
@@ -6,10 +6,135 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Home Page'),
+        backgroundColor: Colors.white,
+        title: const Text(
+          'GDSC BOOKSTORE',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0,
+        leading: const Icon(
+          Icons.sort,
+          color: Colors.black,
+        ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              //color: Colors.grey,
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(18.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Looking for...',
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Icon(Icons.search),
+                  ),
+                  Container(
+                    color: Colors.blueAccent,
+                    child: const Icon(Icons.tune),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Spacer(),
+                          Text(
+                            'sep 23, 2023',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          Icons.pause,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Today a reader ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Icon(
+                          Icons.pause,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'tomorrow a ',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'LEADER',
+                          style: TextStyle(
+                            fontSize: 35,
+                            color: Colors.white,
+                            letterSpacing: 3,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/Path.png'),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- gdsc_bookstore/lib/main.dart
+++ gdsc_bookstore/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gdsc_bookstore/home_page.dart';
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
 }
